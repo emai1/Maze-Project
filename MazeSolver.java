be28f07bd955a71c7fc2b444b2beb75be80b9033
Brian <hlee5@stuy.edu>
2018-04-11 22:00:52 -0400

    Version 0 (for real)

---
+++ MazeSolver.java
@@ -0,0 +1,42 @@
+/*
+    MazeSolver v0
+
+    Given a maze, will return whether the treasure can be reached or not as a boolean value.
+*/
+
+public class MazeSolver {
+
+    private Maze maze;
+    private int[] directions = {Maze.EAST, Maze.SOUTH, Maze.NORTH, Maze.WEST};
+
+    public MazeSolver(Maze input) {
+        maze = new Maze(input);
+    }
+
+    public boolean solve() {
+        
+        // base cases
+        if (maze.explorerIsOnA() == maze.TREASURE) {
+            return true;
+        }
+        if (maze.explorerIsOnA() == maze.WALL) {
+            return false;
+        }
+        
+        // recursive case
+        maze.dropA(maze.WALL);
+        Maze snapshot = new Maze(maze);
+        
+        for (int direction : directions) {
+            maze.go(direction);
+            if (solve()){
+                return true;
+            }
+            else {
+                maze = new Maze(snapshot);
+            }
+        }
+        return false;
+    }
+    
+}
---
+++ UserOfMazeSolver.java
@@ -0,0 +1,29 @@
+/* 
+    Class to test MazeSolver
+*/
+
+
+public class UserOfMazeSolver {
+    
+    public static void main(String[] args) throws java.io.FileNotFoundException {
+
+        // System.out.println("=====Base Case Tests=====");
+
+        // test("mazes\\4cell_treasureWest.txt", 0, 3, false);
+        // test("mazes\\4cell_treasureWest.txt", 0, 0, true);
+
+        System.out.println("=====Recursive Case Tests=====");
+
+        test("mazes\\4cell_treasureWest.txt", 0, 2, true);
+        test("mazes\\4cell_treasureWest.txt", 0, 1, true);
+        test("mazes\\intersection_noTreasure.txt", 1, 1, false);
+        test("mazes\\intersection_treasureNorth.txt", 1, 1, true);
+        test("mazes\\largerMaze.txt", 0, 5, true);
+    }
+
+    private static void test(String mazeName, int startRank, int startFile, boolean answer) throws java.io.FileNotFoundException {
+        Maze maze = new Maze(mazeName, startRank, startFile);
+        MazeSolver solver = new MazeSolver(maze);
+        System.out.println(solver.solve() + ", should be " + answer);
+    }
+}
